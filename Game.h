b87ff2fe34168e83d27a72637f5f5f11337120a7
Fenix2033 <Fenix203317@gmail.com>
2021-11-14 12:26:21 +0100

    Creating class Game and creating a method for Game

---
+++ Game.h
@@ -0,0 +1,21 @@
+//
+// Created by xkudla on 14/11/2021.
+//
+
+#ifndef PROJECT_GAME_H
+#define PROJECT_GAME_H
+#include "Grid.h"
+#include "Position"
+
+class Game {
+    Grid* m_grid;
+    void printIntroduction() const;
+    void printOptions() const;
+    char waitForInput() const;
+    void printEnd() const;
+    void processInput(char input);
+    Position choicePosition();
+};
+
+
+#endif //PROJECT_GAME_H
