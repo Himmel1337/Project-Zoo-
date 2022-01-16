1a1d7fb62502417d7d876a2b39e25914928e7fff
PanSalt <xkochnev@mendelu.cz>
2022-01-16 19:41:00 +0100

    Create class Capitol

---
+++ AbstractBuilding/Capitol.cpp
@@ -0,0 +1,18 @@
+//
+// Created by pco4n on 16.01.2022.
+//
+
+#include "Capitol.h"
+
+int Capitol::getType() {
+    return 1;
+}
+
+std::array<int, 3> Capitol:: getPrice() {
+    std::array<int, 3> price = {12000, 700, 700};
+    return price;
+}
+
+int Capitol::getProfit() {
+    return 5000;
+}
---
+++ AbstractBuilding/Capitol.h
@@ -0,0 +1,17 @@
+//
+// Created by pco4n on 16.01.2022.
+//
+
+#ifndef PROJECT_ZOO_CAPITOL_H
+#define PROJECT_ZOO_CAPITOL_H
+#include "AbstractBuilding.h"
+
+class Capitol: public AbstractBuilding{
+public:
+    int getType() override;
+    std::array<int, 3> getPrice() override;
+    int getProfit() override;
+};
+
+
+#endif //PROJECT_ZOO_CAPITOL_H
